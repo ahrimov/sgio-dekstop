493bde186d3d3a840a7e6007976794d89f57c391
Akhrimov <ahrimovanton@gmail.com>
2026-03-30 00:19:04 +0300

    fix

--- src/components/InfoAttributeView/components/FeatureActionButtons.jsx
+++ src/components/InfoAttributeView/components/FeatureActionButtons.jsx
@@ -21,11 +21,7 @@ export function FeatureActionButtons({
 	if (isNewFeature) {
 		return (
 			<Flex gap={2} justify="center" style={{ width: '100%' }}>
-				<BaseMapButton
-					title="Сохранить"
-					img={saveIcon}
-					onClick={handleSaveEdit}
-				/>
+				<BaseMapButton title="Сохранить" img={saveIcon} onClick={handleSaveEdit} />
 				<BaseMapButton
 					title="Удалить объект"
 					img={deleteIcon}
@@ -37,36 +33,20 @@ export function FeatureActionButtons({
 
 	return (
 		<Flex gap={2} justify="flex-start">
-			<BaseMapButton
-				title="Сохранить"
-				img={saveIcon}
-				onClick={handleSaveEdit}
-			/>
-			<BaseMapButton
-				title="Отменить изменения"
-				img={undoIcon}
-				onClick={handleCancelEdit}
-			/>
+			<BaseMapButton title="Сохранить" img={saveIcon} onClick={handleSaveEdit} />
+			<BaseMapButton title="Отменить изменения" img={undoIcon} onClick={handleCancelEdit} />
 			<BaseMapButton
 				title="Редактировать геометрию"
 				img={editGeometry}
 				onClick={handleEditGeometryClick}
 				active={isGeometryEditing}
 			/>
+			<BaseMapButton title="Удалить объект" img={deleteIcon} onClick={handleDeleteFeature} />
+			<BaseMapButton title="Экспорт в KML" img={exportIcon} onClick={handleExportKML} />
 			<BaseMapButton
 				title="Показать на карте"
 				img={showOnMapIcon}
 				onClick={handleShowOnMap}
-			/>
-			<BaseMapButton
-				title="Экспорт в KML"
-				img={exportIcon}
-				onClick={handleExportKML}
-			/>
-			<BaseMapButton
-				title="Удалить объект"
-				img={deleteIcon}
-				onClick={handleDeleteFeature}
 			/>
 		</Flex>
 	);
